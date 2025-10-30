34e41bf9f7995ec7efa8ae638d6b37d8fac7449f
dev-dede <tsewayog@gmail.com>
2025-10-30 11:15:47 +0000

    Add styling for book and footer section

---
+++ html-css-large-projects/03-coffee-shop/js/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        }
+    },
+});
